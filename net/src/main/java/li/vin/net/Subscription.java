88ffadd6f190a65329a79872b7b208fb5f6d831e
Kyle Turney <kyle@dialexa.com>
2015-06-05 13:15:27 -0500

    edit and create subscriptions

--- net/src/main/java/li/vin/net/Subscription.java
+++ net/src/main/java/li/vin/net/Subscription.java
@@ -2,9 +2,14 @@ package li.vin.net;
 
 import android.support.annotation.Nullable;
 
+import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.TypeAdapter;
 import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
 
+import java.io.IOException;
 import java.lang.reflect.Type;
 
 import auto.parcel.AutoParcel;
@@ -18,10 +23,13 @@ public abstract class Subscription implements VinliItem {
     gb.registerTypeAdapter(Subscription.class, AutoParcelAdapter.create(AutoParcel_Subscription.class));
     gb.registerTypeAdapter(Links.class, AutoParcelAdapter.create(AutoParcel_Subscription_Links.class));
     gb.registerTypeAdapter(PAGE_TYPE, Page.Adapter.create(PAGE_TYPE, Subscription.class));
+
+    gb.registerTypeAdapter(SeedCreate.class, new SeedCreate.Adapter());
+    gb.registerTypeAdapter(SeedEdit.class, new SeedEdit.Adapter());
   }
 
-  /*package*/ static final Builder builder() {
-    return new AutoParcel_Subscription.Builder();
+  public static final SeedCreate.Builder create() {
+    return new AutoParcel_Subscription_SeedCreate.Builder();
   }
 
   public abstract String deviceId();
@@ -39,6 +47,14 @@ public abstract class Subscription implements VinliItem {
 
   public Observable<Page<Notification>> notifications() {
     return Vinli.curApp().linkLoader().read(links().notifications(), Notification.PAGE_TYPE);
+  }
+
+  public SeedEdit.Builder edit() {
+    return new AutoParcel_Subscription_SeedEdit.Builder()
+        .appData(appData())
+        .url(url())
+        .deviceId(deviceId())
+        .subscriptionId(id());
   }
 
   public Observable<Void> delete() {
@@ -76,5 +92,103 @@ public abstract class Subscription implements VinliItem {
     Builder links(Links l);
 
     Subscription build();
+  }
+
+  @AutoParcel
+  public static abstract class SeedCreate {
+    public abstract String eventType();
+    @Nullable public abstract ObjectRef object();
+    public abstract String url();
+    @Nullable public abstract String appData();
+    public abstract String deviceId();
+
+    /*package*/ SeedCreate() { }
+
+    @AutoParcel.Builder
+    public static abstract class Builder {
+      public abstract Builder eventType(String s);
+      public abstract Builder object(@Nullable ObjectRef o);
+      public abstract Builder url(String s);
+      public abstract Builder appData(@Nullable String s);
+      public abstract Builder deviceId(String deviceId);
+
+      /*package*/ Builder() { }
+
+      /*package*/ abstract SeedCreate autoBuild();
+
+      public Observable<Subscription> save() {
+        final SeedCreate sc = autoBuild();
+
+        return Vinli.curApp().subscriptions().create(sc.deviceId(), sc)
+            .map(Wrapped.<Subscription>pluckItem());
+      }
+    }
+
+    /*package*/ static final class Adapter extends TypeAdapter<SeedCreate> {
+      private Gson gson;
+
+      @Override public void write(JsonWriter out, SeedCreate value) throws IOException {
+        if (gson == null) {
+          gson = Vinli.curApp().gson();
+        }
+
+        out.beginObject();
+          out.name("subscription").beginObject();
+            out.name("eventType").value(value.eventType());
+            out.name("object"); gson.toJson(value.object(), ObjectRef.class, out);
+            out.name("url").value(value.url());
+            out.name("appData").value(value.appData());
+          out.endObject();
+        out.endObject();
+      }
+
+      @Override public SeedCreate read(JsonReader in) throws IOException {
+        throw new UnsupportedOperationException("reading a Subscription.SeedCreate is not supported");
+      }
+    }
+  }
+
+  @AutoParcel
+  public static abstract class SeedEdit {
+    public abstract String url();
+    @Nullable public abstract String appData();
+    /*package*/ abstract String deviceId();
+    /*package*/ abstract String subscriptionId();
+
+    /*package*/ SeedEdit() { }
+
+    @AutoParcel.Builder
+    public static abstract class Builder {
+      public abstract Builder url(String s);
+      public abstract Builder appData(@Nullable String s);
+      /*package*/ abstract Builder deviceId(String deviceId);
+      /*package*/ abstract Builder subscriptionId(String deviceId);
+
+      /*package*/ Builder() { }
+
+      /*package*/ abstract SeedEdit autoBuild();
+
+      public Observable<Subscription> save() {
+        final SeedEdit se = autoBuild();
+
+        return Vinli.curApp().subscriptions().edit(se.deviceId(), se.subscriptionId(), se)
+            .map(Wrapped.<Subscription>pluckItem());
+      }
+    }
+
+    /*package*/ static final class Adapter extends TypeAdapter<SeedEdit> {
+      @Override public void write(JsonWriter out, SeedEdit value) throws IOException {
+        out.beginObject();
+          out.name("subscription").beginObject();
+            out.name("url").value(value.url());
+            out.name("appData").value(value.appData());
+          out.endObject();
+        out.endObject();
+      }
+
+      @Override public SeedEdit read(JsonReader in) throws IOException {
+        throw new UnsupportedOperationException("reading a Subscription.SeedEdit is not supported");
+      }
+    }
   }
 }
--- net/src/main/java/li/vin/net/Subscriptions.java
+++ net/src/main/java/li/vin/net/Subscriptions.java
@@ -3,7 +3,10 @@ package li.vin.net;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
+import retrofit.http.Body;
 import retrofit.http.GET;
+import retrofit.http.POST;
+import retrofit.http.PUT;
 import retrofit.http.Path;
 import retrofit.http.Query;
 import rx.Observable;
@@ -22,5 +25,16 @@ import rx.Observable;
   Observable<Wrapped<Subscription>> subscription(
       @NonNull @Path("deviceId") String deviceId,
       @NonNull @Path("subscriptionId") String subscriptionId);
+
+  @POST("/devices/{deviceId}/subscriptions")
+  Observable<Wrapped<Subscription>> create(
+      @NonNull @Path("deviceId") String deviceId,
+      @NonNull @Body Subscription.SeedCreate seedCreate);
+
+  @PUT("/devices/{deviceId}/subscriptions/{subscriptionId}")
+  Observable<Wrapped<Subscription>> edit(
+      @NonNull @Path("deviceId") String deviceId,
+      @NonNull @Path("subscriptionId") String subscriptionId,
+      @NonNull @Body Subscription.SeedEdit seedEdit);
 
 }
