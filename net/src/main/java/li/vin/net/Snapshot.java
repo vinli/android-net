972b9c615a215e93184e2298419b32bd76b12be8
Kyle Turney <kyle@dialexa.com>
2015-06-08 10:55:23 -0500

    add Message

--- net/src/main/java/li/vin/net/Snapshot.java
+++ net/src/main/java/li/vin/net/Snapshot.java
@@ -1,9 +1,7 @@
 package li.vin.net;
 
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
-import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.TypeAdapter;
 import com.google.gson.reflect.TypeToken;
@@ -18,7 +16,7 @@ import java.util.Map;
 import auto.parcel.AutoParcel;
 
 @AutoParcel
-public abstract class Snapshot implements VinliItem {
+public abstract class Snapshot extends DataItem {
   /*package*/ static final Type TIME_SERIES_TYPE = new TypeToken<TimeSeries<Snapshot>>() { }.getType();
 
   /*package*/ static final void registerGson(GsonBuilder gb) {
@@ -27,26 +25,6 @@ public abstract class Snapshot implements VinliItem {
   }
 
   public abstract String timestamp();
-
-  @Nullable /*package*/ abstract Map<String, String> data();
-
-  public boolean hasValue(@NonNull String name) {
-    final Map<String, String> data = data();
-    if (data == null) {
-      return false;
-    }
-
-    return data.containsKey(name);
-  }
-
-  @Nullable public String value(@NonNull String name) {
-    final Map<String, String> data = data();
-    if (data == null) {
-      return null;
-    }
-
-    return data.get(name);
-  }
 
   /*package*/ Snapshot() { }
 
@@ -60,17 +38,11 @@ public abstract class Snapshot implements VinliItem {
   }
 
   private static final class Adapter extends TypeAdapter<Snapshot> {
-    private Gson gson;
-
     @Override public void write(JsonWriter out, Snapshot value) throws IOException {
       throw new UnsupportedOperationException("writing a Snapshot is not supported");
     }
 
     @Override public Snapshot read(JsonReader in) throws IOException {
-      if (gson == null) {
-        gson = Vinli.curApp().gson();
-      }
-
       final Snapshot.Builder b = new AutoParcel_Snapshot.Builder();
       Map<String, String> data = null;
 
