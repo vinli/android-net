d8bf179fa939b5b57b2e089354c5c8ac12a2c571
Josh Beridon <jberidon@gmail.com>
2015-06-17 15:29:03 -0500

    object ref is now public facing

--- net/src/main/java/li/vin/net/ObjectRef.java
+++ net/src/main/java/li/vin/net/ObjectRef.java
@@ -19,10 +19,11 @@ public abstract class ObjectRef implements VinliItem {
   /*package*/ ObjectRef() { }
 
   @AutoParcel.Builder
-  /*package*/ interface Builder {
-    Builder id(String s);
-    Builder type(String s);
+  /*package*/ public static abstract class Builder{
+    public abstract Builder id(String s);
+    public abstract Builder type(String s);
 
-    ObjectRef build();
+    public abstract ObjectRef build();
+  /*package*/ Builder(){  }
   }
 }
