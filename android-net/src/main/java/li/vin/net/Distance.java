93c9006104f1ff6890ae6d53cd0c71093cecf9eb
Tommy Brown <thomasbrown73095@gmail.com>
2016-03-22 13:25:08 -0500

    Added body to Distance

--- android-net/src/main/java/li/vin/net/Distance.java
+++ android-net/src/main/java/li/vin/net/Distance.java
@@ -1,7 +1,21 @@
 package li.vin.net;
 
+import auto.parcel.AutoParcel;
+import com.google.gson.GsonBuilder;
+
 /**
  * Created by tbrown on 3/22/16.
  */
+
+@AutoParcel
 public abstract class Distance implements VinliItem{
+
+  /*package*/ static final void registerGson(GsonBuilder gb) {
+    gb.registerTypeAdapter(Distance.class, AutoParcelAdapter.create(AutoParcel_Distance.class));
+  }
+
+  public abstract Double confidenceMin();
+  public abstract Double confidenceMax();
+  public abstract Double value();
+  public abstract String lastOdometerDate();
 }
--- android-net/src/main/java/li/vin/net/Distances.java
+++ android-net/src/main/java/li/vin/net/Distances.java
@@ -1,10 +1,12 @@
 package li.vin.net;
 
+import java.util.List;
 import retrofit.http.Body;
 import retrofit.http.DELETE;
-import retrofit.http.Field;
 import retrofit.http.GET;
 import retrofit.http.POST;
+import retrofit.http.Path;
+import retrofit.http.Query;
 import rx.Observable;
 
 /**
@@ -13,29 +15,42 @@ import rx.Observable;
 public interface Distances {
 
   @GET("/vehicles/{vehicleId}/distances")
-  Observable<Object> distances(@Field("vehicleId") String vehicleId);
+  Observable<List<Distance>> distances(
+      @Path("vehicleId") String vehicleId,
+      @Query("from") String from,
+      @Query("until") String until);
 
   @POST("/vehicles/{vehicleId}/odometers")
-  Observable<Object> createOdometerReport(@Field("vehicleId") String vehicleId, @Body Object odometer);
+  Observable<Object> createOdometerReport(
+      @Path("vehicleId") String vehicleId,
+      @Body Object odometer);
 
   @GET("/vehicles/{vehicleId}/odometers")
-  Observable<Object> odometerReports(@Field("vehicleId") String vehicleId);
+  Observable<Object> odometerReports(
+      @Path("vehicleId") String vehicleId);
 
   @GET("/odometers/{odometerId}")
-  Observable<Object> odometerReport(@Field("odometerId") String odometerId);
+  Observable<Object> odometerReport(
+      @Path("odometerId") String odometerId);
 
   @DELETE("/odometers/{odometerId}")
-  Observable<Object> deleteOdometerReport(@Field("odometerId") String odometerId);
+  Observable<Object> deleteOdometerReport(
+      @Path("odometerId") String odometerId);
 
   @POST("/vehicles/{vehicleId}/odometer_triggers")
-  Observable<Object> createOdometerTrigger(@Field("vehicleId") String vehicleId, @Body Object odometerTrigger);
+  Observable<Object> createOdometerTrigger(
+      @Path("vehicleId") String vehicleId,
+      @Body Object odometerTrigger);
 
   @GET("/odometer_triggers/{odometerTriggerId}")
-  Observable<Object> odometerTrigger(@Field("odometerTriggerId") String odometerTriggerId);
+  Observable<Object> odometerTrigger(
+      @Path("odometerTriggerId") String odometerTriggerId);
 
   @DELETE("/odometer_triggers/{odometerTriggerId}")
-  Observable<Object> deleteOdometerTrigger(@Field("odometerTriggerId") String odometerTriggerId);
+  Observable<Object> deleteOdometerTrigger(
+      @Path("odometerTriggerId") String odometerTriggerId);
 
   @GET("/vehicles/{vehicleId}/odometer_triggers")
-  Observable<Object> odometerTriggers(@Field("vehicleId") String vehicleId);
+  Observable<Object> odometerTriggers(
+      @Path("vehicleId") String vehicleId);
 }
